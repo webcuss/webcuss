67ee560d3d91e93cd839105a54759848d95b8ca5
dev-sareno <dev-sareno@users.noreply.github.com>
2022-10-16 13:11:58 +0800

    refactor: move to page-info component

---
+++ extension/src/components/page-info/PageInfo.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+const PageInfo = () => {
+    const [url, setUrl] = useState<string>("");
+    const [title, setTitle] = useState<string>("");
+    const [scheme, setScheme] = useState<string>("");
+    const [hostname, setHostname] = useState<string>("");
+    const [path, setPath] = useState<string>("");
+    const [query, setQuery] = useState<string>("");
+
+    useEffect(() => {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            const currentTab = tabs[0];
+            const tabUrl = currentTab.url;
+            if (tabUrl) {
+                setUrl(tabUrl);
+                const u = new URL(tabUrl);
+                setScheme(u.protocol);
+                setHostname(u.hostname);
+                setPath(u.pathname);
+                setQuery(u.search);
+            }
+            if (currentTab.title) {
+                setTitle(currentTab.title);
+            }
+        });
+    }, []);
+
+    return (
+        <div>
+            <div>URL: {url}</div>
+            <div>Title: {title}</div>
+            <div>Scheme: {scheme}</div>
+            <div>Hostname: {hostname}</div>
+            <div>Path: {path}</div>
+            <div>Query: {query}</div>
+        </div>
+    );
+};
+
+export default PageInfo;
